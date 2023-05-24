7d8a7279dcd610eb28077343a3fce5c108855b61
Aline dAvila <aline_davila@live.com>
2023-05-24 16:41:27 -0300

    adicionada a pasta src/

---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,47 @@
+const screen = {
+    userProfile: document.querySelector(".profile-data"), 
+    renderUser(user){
+        this.userProfile.innerHTML = `<div class="info"> 
+                                        <img src="${user.avatarUrl}" alt="Foto do perfil do usuário"/> 
+                                            <div class="data">
+                                                <h1>${user.name?? 'Não possui nome cadastrado :(' }</h1>
+                                                <p>${user.bio?? 'Não possui nome cadastrado :(' }</p><br>
+                                                <p>Followers: ${user.followers}</p>
+                                                <p>Following: ${user.following}</p>
+                                            </div> 
+                                      </div>`
+
+
+        let repositoriesItems = ''
+        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}"                                                      target="_blank">${repo.name}<br><br>
+            <span> <img src="./src/images/forks.png" alt="forks"> ${repo.forks_count}</span>  
+            <span> <img src="./src/images/star.png" alt="star"> ${repo.stargazers_count}</span>
+            <span> <img src="./src/images/eyes.png" alt="eyes"> ${repo.watchers_count}</span>
+            <span> <img src="./src/images/work.png" alt="working on a computer"> ${repo.language?? ' '}</span>
+                                                                </a></li>`
+    )
+            this.userProfile.innerHTML += `<div class="repositories section">
+                                                <h2>Repositórios</h2>
+                                                <ul>${repositoriesItems}</ul>
+                                           </div>`
+
+
+        let eventItems = ''
+            user.moves.forEach((move) => { 
+                if(move.type === 'PushEvent'){
+                    eventItems += `<li> <span> ${move.repo.name} </span> - ${move.payload.commits[0].message} </li>`
+                }
+        })
+
+        this.userProfile.innerHTML += `<div class="events section">
+                                            <h2>Eventos</h2>
+                                            <ul> ${eventItems} </ul>
+                                      </div>`
+    },
+
+    renderNotFound(){
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
+    }
+}
+
+export{ screen }
